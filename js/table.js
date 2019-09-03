256cac931b1d0910e75bc45b458da0676f290aaa
kuro08 <kuroschaeffer@gmail.com>
2019-09-03 19:22:14 +0700

    table updated

--- js/table.js
+++ js/table.js
@@ -21,11 +21,7 @@ function arrayData() {
         Star: 4.37,
         PP: 78,
         Rank: "S",
-<<<<<<< HEAD
-        Ranking: "soon",
-=======
         Ranking: "#30",
->>>>>>> 0564fbf1b01d08eb3dc6d5f91e30988aaa6b7e8b
         Mod: "NM",
         Status: "Pass"
     }, {
@@ -33,11 +29,7 @@ function arrayData() {
         Star: 4.37,
         PP: 52,
         Rank: "S",
-<<<<<<< HEAD
-        Ranking: "soon",
-=======
         Ranking: "#43",
->>>>>>> 0564fbf1b01d08eb3dc6d5f91e30988aaa6b7e8b
         Mod: "HD",
         Status: "Pass"
     }, {
@@ -45,9 +37,7 @@ function arrayData() {
         Star: 5.29,
         PP: 37,
         Rank: "A",
-        Ranking: "soon",
-<<<<<<< HEAD
-=======
+        Ranking: "#587",
         Mod: "NM",
         Status: "Pass"
     },{
@@ -83,20 +73,179 @@ function arrayData() {
         Mod: "NM",
         Status: "Pass"
     },{
-        Map: " Takamori Natsumi, Sendai Eri, Asakawa Yuu, Tanezaki Atsumi - Ne! Ko! [Extra]",
+        Map: "Takamori Natsumi, Sendai Eri, Asakawa Yuu, Tanezaki Atsumi - Ne! Ko! [Extra]",
         Star: 3.36,
         PP: 143,
-        Rank: "S",
+        Rank: "SS",
         Ranking: "#82",
         Mod: "NM",
         Status: "FC"
     },{
-        Map: " Yoshino Nanjo - Kimi o Sagashi ni [Mitsuketa]",
+        Map: "Yoshino Nanjo - Kimi o Sagashi ni [Mitsuketa]",
         Star: 3.60,
         PP: 130,
         Rank: "S",
         Ranking: "#13",
->>>>>>> 0564fbf1b01d08eb3dc6d5f91e30988aaa6b7e8b
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "CLIFF EDGE feat. Nakamura Maiko - The Distance [Master]",
+        Star: 3.49,
+        PP: "&hearts;",
+        Rank: "S",
+        Ranking: "#53",
+        Mod: "NF",
+        Status: "Pass NF"
+    },{
+        Map: "Porter Robinson - Flicker [Worlds]",
+        Star: 3.48,
+        PP: 95,
+        Rank: "S",
+        Ranking: "#247",
+        Mod: "NF",
+        Status: "Pass NF"
+    },{
+        Map: "ChouCho - BLESS YoUr NAME [lfj's Insane] ",
+        Star: 3.44,
+        PP: 88,
+        Rank: "S",
+        Ranking: "#192",
+        Mod: "NF",
+        Status: "Pass NF"
+    },{
+        Map: "Tamura Yukari, Kanai Mika & Horie Yui - Happy! Lucky! Dochy! [Collab]",
+        Star: 3.38,
+        PP: 75,
+        Rank: "S",
+        Ranking: "#102",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "Mayama Rika - Liar Mask anime size [Insane]",
+        Star: 3.38,
+        PP: 78,
+        Rank: "S",
+        Ranking: "#24",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "Tomita Miyu, Onishi Saori, Ohzora Naomi, Hanazawa Kana - Gabriel Drop Kick [Insane]",
+        Star: 3.35,
+        PP: 44,
+        Rank: "S",
+        Ranking: "#208",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "L.E.D.-G + Qrispy Joybox + ARM feat. Nanahira - Dokkin Summer Adventure [Lami's Another]",
+        Star: 3.30,
+        PP: 78,
+        Rank: "S",
+        Ranking: "#85",
+        Mod: "NF",
+        Status: "Pass NF"
+    },{
+        Map: "RADWIMPS - Yume Tourou [Your Name]",
+        Star: 3.19,
+        PP: 105,
+        Rank: "S",
+        Ranking: "#140",
+        Mod: "NM",
+        Status: "FC"
+    },{
+        Map: "Fujiwara Chika (CV: Kohara Konomi) - Chikatto Chika Chika (TV Size) [Amity's Insane]",
+        Star: 3.05,
+        PP: 62,
+        Rank: "S",
+        Ranking: "#248",
+        Mod: "NF",
+        Status: "Pass NF"
+    },{
+        Map: "Chimame-tai - Tokimeki Poporon [Crystal's Insane]",
+        Star: 3.09,
+        PP: 54,
+        Rank: "S",
+        Ranking: "#109",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "FROZEN QUALIA - Kaleidoscope [Lasse's Petal]",
+        Star: 3.00,
+        PP: 33,
+        Rank: "S",
+        Ranking: "#9",
+        Mod: "NF",
+        Status: "Pass NF"
+    },{
+        Map: "Nanahira - Youkoso Japari Park e [gary00737's Insane]",
+        Star: 2.82,
+        PP: 50,
+        Rank: "S",
+        Ranking: "#73",
+        Mod: "NF",
+        Status: "Pass"
+    },{
+        Map: "Katakiri Rekka & Suzuyu - Girl meets Love [Insane]",
+        Star: 2.75,
+        PP: 72,
+        Rank: "SS",
+        Ranking: "#276",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "Chimame-tai - Tokimeki Poporon [Crystal's Insane]",
+        Star: 3.09,
+        PP: 54,
+        Rank: "S",
+        Ranking: "#109",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "Chimame-tai - Tokimeki Poporon [Crystal's Insane]",
+        Star: 3.09,
+        PP: 54,
+        Rank: "S",
+        Ranking: "#109",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "Chimame-tai - Tokimeki Poporon [Crystal's Insane]",
+        Star: 3.09,
+        PP: 54,
+        Rank: "S",
+        Ranking: "#109",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "Chimame-tai - Tokimeki Poporon [Crystal's Insane]",
+        Star: 3.09,
+        PP: 54,
+        Rank: "S",
+        Ranking: "#109",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "Chimame-tai - Tokimeki Poporon [Crystal's Insane]",
+        Star: 3.09,
+        PP: 54,
+        Rank: "S",
+        Ranking: "#109",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "Chimame-tai - Tokimeki Poporon [Crystal's Insane]",
+        Star: 3.09,
+        PP: 54,
+        Rank: "S",
+        Ranking: "#109",
+        Mod: "NM",
+        Status: "Pass"
+    },{
+        Map: "Chimame-tai - Tokimeki Poporon [Crystal's Insane]",
+        Star: 3.09,
+        PP: 54,
+        Rank: "S",
+        Ranking: "#109",
         Mod: "NM",
         Status: "Pass"
     }];
